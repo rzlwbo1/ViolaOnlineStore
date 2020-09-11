bcb934db618fdbb07b05463ca70e90ffc83ba591
rzlwbo1 <rizalwb2@gmail.com>
2020-09-11 14:52:08 +0700

    Homepage Selesai

---
+++ script/index.js
@@ -0,0 +1,10 @@
+
+const menuElement = document.querySelector('.menu');
+const linksElement = document.querySelector('.links');
+
+menuElement.addEventListener('click', () => linksElement.classList.toggle('show'));
+console.log(linksElement);
+
+
+
+
