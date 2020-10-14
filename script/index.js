5298db11d933906ad1f23e8d923c01063c913fc4
rzlwbo1 <rizalwb2@gmail.com>
2020-10-14 15:23:36 +0700

    fixed navbar

--- script/index.js
+++ script/index.js
@@ -2,8 +2,17 @@
 const menuElement = document.querySelector('.menu');
 const linksElement = document.querySelector('.links');
 
-menuElement.addEventListener('click', () => linksElement.classList.toggle('show'));
-console.log(linksElement);
+menuElement.addEventListener('click', openNav);
+
+function openNav(e) {
+    document.body.style.overflowY = 'hidden';
+    linksElement.classList.toggle('show');
+    if(linksElement.getAttribute('class') == 'links') {
+        document.body.style.overflow = 'auto';
+    }
+}
+
+console.log(linksElement.getAttribute('class'));
 
 
 
